8fd5e3a08f96dff819f139d6751cc080860b8b81
ayslanms@gmail.com <ayslanms@gmail.com@e489019e-bc00-2c72-aadb-f79410f744ba>
2013-08-09 14:01:07 +0000

    git-svn-id: http://xpert-framework.googlecode.com/svn/xpert-framework/trunk@216 e489019e-bc00-2c72-aadb-f79410f744ba

--- src/main/java/com/xpert/persistence/query/Restrictions.java
+++ src/main/java/com/xpert/persistence/query/Restrictions.java
@@ -10,7 +10,7 @@ import javax.persistence.TemporalType;
  * @author Ayslan
  */
 public class Restrictions extends ArrayList<Restriction> {
-    
+
     public Restrictions add(String property, RestrictionType restrictionType) {
         this.add(new Restriction(property, restrictionType));
         return this;
@@ -38,6 +38,136 @@ public class Restrictions extends ArrayList<Restriction> {
 
     public Restrictions add(String property, RestrictionType restrictionType, Object value, LikeType likeType) {
         this.add(new Restriction(property, restrictionType, value, likeType));
+        return this;
+    }
+
+    public Restrictions isNull(String property) {
+        this.add(new Restriction(property, RestrictionType.NULL));
+        return this;
+    }
+
+    public Restrictions isNotNull(String property) {
+        this.add(new Restriction(property, RestrictionType.NOT_NULL));
+        return this;
+    }
+
+    public Restrictions like(String property, Object value) {
+        this.add(new Restriction(property, RestrictionType.LIKE, value));
+        return this;
+    }
+
+    public Restrictions like(String property, Object value, LikeType likeType) {
+        this.add(new Restriction(property, RestrictionType.LIKE, value, likeType));
+        return this;
+    }
+
+    public Restrictions notLike(String property, Object value) {
+        this.add(new Restriction(property, RestrictionType.NOT_LIKE, value));
+        return this;
+    }
+
+    public Restrictions notLike(String property, Object value, LikeType likeType) {
+        this.add(new Restriction(property, RestrictionType.NOT_LIKE, value, likeType));
+        return this;
+    }
+
+    public Restrictions greaterThan(String property, Object value) {
+        this.add(new Restriction(property, RestrictionType.GREATER_THAN, value));
+        return this;
+    }
+
+    public Restrictions greaterThan(String property, Date value, TemporalType temporalType) {
+        this.add(new Restriction(property, RestrictionType.GREATER_THAN, value, temporalType));
+        return this;
+    }
+
+    public Restrictions greaterThan(String property, Calendar value, TemporalType temporalType) {
+        this.add(new Restriction(property, RestrictionType.GREATER_THAN, value, temporalType));
+        return this;
+    }
+
+    public Restrictions greaterEqualsThan(String property, Object value) {
+        this.add(new Restriction(property, RestrictionType.GREATER_EQUALS_THAN, value));
+        return this;
+    }
+
+    public Restrictions greaterEqualsThan(String property, Date value, TemporalType temporalType) {
+        this.add(new Restriction(property, RestrictionType.GREATER_EQUALS_THAN, value, temporalType));
+        return this;
+    }
+
+    public Restrictions greaterEqualsThan(String property, Calendar value, TemporalType temporalType) {
+        this.add(new Restriction(property, RestrictionType.GREATER_EQUALS_THAN, value, temporalType));
+        return this;
+    }
+
+    public Restrictions lessThan(String property, Object value) {
+        this.add(new Restriction(property, RestrictionType.LESS_THAN, value));
+        return this;
+    }
+
+    public Restrictions lessThan(String property, Date value, TemporalType temporalType) {
+        this.add(new Restriction(property, RestrictionType.LESS_THAN, value, temporalType));
+        return this;
+    }
+
+    public Restrictions lessThan(String property, Calendar value, TemporalType temporalType) {
+        this.add(new Restriction(property, RestrictionType.LESS_THAN, value, temporalType));
+        return this;
+    }
+
+    public Restrictions lessEqualsThan(String property, Object value) {
+        this.add(new Restriction(property, RestrictionType.LESS_EQUALS_THAN, value));
+        return this;
+    }
+
+    public Restrictions lessEqualsThan(String property, Date value, TemporalType temporalType) {
+        this.add(new Restriction(property, RestrictionType.LESS_EQUALS_THAN, value, temporalType));
+        return this;
+    }
+
+    public Restrictions lessEqualsThan(String property, Calendar value, TemporalType temporalType) {
+        this.add(new Restriction(property, RestrictionType.LESS_EQUALS_THAN, value, temporalType));
+        return this;
+    }
+
+    public Restrictions in(String property, Object value) {
+        this.add(new Restriction(property, RestrictionType.IN, value));
+        return this;
+    }
+
+    public Restrictions notIn(String property, Object value) {
+        this.add(new Restriction(property, RestrictionType.NOT_IN, value));
+        return this;
+    }
+
+    public Restrictions equals(String property, Object value) {
+        this.add(new Restriction(property, RestrictionType.EQUALS, value));
+        return this;
+    }
+
+    public Restrictions equals(String property, Date value, TemporalType temporalType) {
+        this.add(new Restriction(property, RestrictionType.EQUALS, value, temporalType));
+        return this;
+    }
+
+    public Restrictions equals(String property, Calendar value, TemporalType temporalType) {
+        this.add(new Restriction(property, RestrictionType.EQUALS, value, temporalType));
+        return this;
+    }
+
+    public Restrictions notEquals(String property, Object value) {
+        this.add(new Restriction(property, RestrictionType.NOT_EQUALS, value));
+        return this;
+    }
+
+    public Restrictions notEquals(String property, Date value, TemporalType temporalType) {
+        this.add(new Restriction(property, RestrictionType.NOT_EQUALS, value, temporalType));
+        return this;
+    }
+
+    public Restrictions notEquals(String property, Calendar value, TemporalType temporalType) {
+        this.add(new Restriction(property, RestrictionType.NOT_EQUALS, value, temporalType));
         return this;
     }
 }
