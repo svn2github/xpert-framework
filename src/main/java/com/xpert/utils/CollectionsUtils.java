c0a86064e595358224be2268ff77ac7afd297116
arnaldofox@gmail.com <arnaldofox@gmail.com@e489019e-bc00-2c72-aadb-f79410f744ba>
2013-09-24 14:21:17 +0000

    git-svn-id: http://xpert-framework.googlecode.com/svn/xpert-framework/trunk@231 e489019e-bc00-2c72-aadb-f79410f744ba

--- src/main/java/com/xpert/utils/CollectionsUtils.java
+++ src/main/java/com/xpert/utils/CollectionsUtils.java
@@ -24,6 +24,20 @@ public class CollectionsUtils {
         multiSort.setReverseSort(0);
         Collections.sort(list, multiSort);
     }
-
-
+    
+    @SuppressWarnings("unchecked")
+    public static void order(List list, String ordenacao) {
+        ComparatorChain multiSort = new ComparatorChain();
+        String array[] = ordenacao.split(",");
+        for (int i = 0; i < array.length; i++) {
+            String field[] = array[i].trim().split(" ");
+            if (field.length == 1 || (field.length == 2 && field[1].endsWith("asc"))) {
+                multiSort.addComparator(new BeanComparator(field[0].trim()), false);
+            } else {
+                multiSort.addComparator(new BeanComparator(field[0].trim()), true);
+            }
+        }
+        Collections.sort(list, multiSort);
+    }
+    
 }
