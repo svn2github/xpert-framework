fe918acdbec072c46a2bcb331d065a1129799e6d
arnaldofox@gmail.com <arnaldofox@gmail.com@e489019e-bc00-2c72-aadb-f79410f744ba>
2013-09-24 21:48:13 +0000

    git-svn-id: http://xpert-framework.googlecode.com/svn/xpert-framework/trunk@232 e489019e-bc00-2c72-aadb-f79410f744ba

--- src/main/java/com/xpert/utils/CollectionsUtils.java
+++ src/main/java/com/xpert/utils/CollectionsUtils.java
@@ -1,43 +1,55 @@
 package com.xpert.utils;
 
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import org.apache.commons.beanutils.BeanComparator;
 import org.apache.commons.collections.comparators.ComparatorChain;
+import org.apache.commons.collections.comparators.NullComparator;
 
 public class CollectionsUtils {
 
     @SuppressWarnings("unchecked")
     public static void orderAsc(List list, String ordenacao) {
-        ArrayList colecao = new ArrayList();
-        colecao.add(new BeanComparator(ordenacao));
-        ComparatorChain multiSort = new ComparatorChain(colecao);
+        ComparatorChain multiSort = new ComparatorChain();
+        String array[] = ordenacao.split(",");
+        for (int i = 0; i < array.length; i++) {
+            multiSort.addComparator(new BeanComparator(array[i].trim(), new NullComparator()), false);
+        }
         Collections.sort(list, multiSort);
     }
 
     @SuppressWarnings("unchecked")
     public static void orderDesc(List list, String ordenacao) {
-        ArrayList colecao = new ArrayList();
-        colecao.add(new BeanComparator(ordenacao));
-        ComparatorChain multiSort = new ComparatorChain(colecao);
-        multiSort.setReverseSort(0);
+        ComparatorChain multiSort = new ComparatorChain();
+        String array[] = ordenacao.split(",");
+        for (int i = 0; i < array.length; i++) {
+            multiSort.addComparator(new BeanComparator(array[i].trim(), new NullComparator()), true);
+        }
         Collections.sort(list, multiSort);
     }
-    
+
+    /**
+     *
+     * @param collection
+     * @param ordenacao
+     * @throws IllegalArgumentException
+     */
     @SuppressWarnings("unchecked")
-    public static void order(List list, String ordenacao) {
+    public static void order(List list, String ordenacao) throws IllegalArgumentException {
         ComparatorChain multiSort = new ComparatorChain();
         String array[] = ordenacao.split(",");
         for (int i = 0; i < array.length; i++) {
             String field[] = array[i].trim().split(" ");
-            if (field.length == 1 || (field.length == 2 && field[1].endsWith("asc"))) {
-                multiSort.addComparator(new BeanComparator(field[0].trim()), false);
+            if (field.length == 1 || (field.length == 2 && field[1].equalsIgnoreCase("asc"))) {
+                multiSort.addComparator(new BeanComparator(field[0].trim(), new NullComparator()), false);
             } else {
-                multiSort.addComparator(new BeanComparator(field[0].trim()), true);
+                if (field[1].equalsIgnoreCase("desc")) {
+                    multiSort.addComparator(new BeanComparator(field[0].trim(), new NullComparator()), true);
+                } else {
+                    throw new IllegalArgumentException("Value of the second argument must be asc or desc");
+                }
             }
         }
         Collections.sort(list, multiSort);
     }
-    
 }
