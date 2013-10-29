6aeed34e8698649dc9b190cc72c93eb872353c01
arnaldofox@gmail.com <arnaldofox@gmail.com@e489019e-bc00-2c72-aadb-f79410f744ba>
2013-10-29 18:25:06 +0000

    git-svn-id: http://xpert-framework.googlecode.com/svn/xpert-framework/trunk@247 e489019e-bc00-2c72-aadb-f79410f744ba

--- src/main/java/com/xpert/utils/Mod11.java
+++ src/main/java/com/xpert/utils/Mod11.java
@@ -6,7 +6,202 @@ package com.xpert.utils;
  */
 public class Mod11 {
 
-   public static String getMod11(String string) {
+    public static String getDV(String number) {
+
+        return getDV(number, false);
+    }
+
+    public static String getDV(String number, int quantidadeDigitos) {
+
+        return getDV(number, false, quantidadeDigitos);
+    }
+
+    /**
+     * Calcular um dígito verificador a partir de uma sequência de números
+     * enviada.
+     *
+     * @param number - Sequência de números para cálculo do DV
+     * @param dezPorX Indica se deve haver substituição de resultado 10 por X
+     * durante o cálculo - padrão usado em alguns lugares
+     * @return DV gerado.
+     */
+    public static String getDV(String number, boolean dezPorX) {
+
+        if (!org.apache.commons.lang.StringUtils.isNumeric(number)) {
+            throw new IllegalArgumentException("Value is not number");
+        }
+
+        int peso = number.length() + 1;
+        int dv = 0;
+        for (int i = 0; i < number.length(); i++) {
+            dv += Integer.parseInt(number.substring(i, i + 1)) * peso--;
+        }
+        dv = dv % 11;
+        if (dv > 1) {
+            return String.valueOf(11 - dv);
+        } else if (dv == 1 && dezPorX) {
+            return "X";
+        }
+        return "0";
+
+    }
+
+    /**
+     * Calcular um dígito verificador com a quantidade de casas indicadas a
+     * partir de uma sequência de números enviada.
+     *
+     * @param number - Sequência de números para cálculo do DV
+     * @param dezPorX Indica se deve haver substituição de resultado 10 por X
+     * durante o cálculo - padrão usado em alguns lugares
+     * @param quantidadeDigitos Quantidade de dígitos a serem retornados
+     * @return DV gerado.
+     */
+    public static String getDV(String number, boolean dezPorX, int quantidadeDigitos) {
+        if (quantidadeDigitos > 1) {
+            String parcial = getDV(number, dezPorX);
+            return parcial + getDV(number + parcial, dezPorX, --quantidadeDigitos);
+        } else {
+            return getDV(number, dezPorX);
+        }
+    }
+
+    /**
+     * Calcular um dígito verificador a partir de uma sequência de números
+     * enviada. O maior peso usado é 9, retornando a 2.
+     *
+     * @param number - Sequência de números para cálculo do DV
+     * @param dezPorX Indica se deve haver substituição de resultado 10 por X
+     * durante o cálculo - padrão usado em alguns lugares
+     * @return DV gerado.
+     */
+    public static String getDVBase10(String number, boolean dezPorX) {
+        char subUm = '0';
+        if (dezPorX) {
+            subUm = 'X';
+        }
+        return getDVBaseParametrizada(number, 10, '0', subUm);
+    }
+
+    /**
+     * Calcular um dígito verificador usando o módulo 11, base 10, com a
+     * quantidade de casas indicadas a partir de uma sequência de números
+     * enviada.
+     *
+     * @param number - Sequência de números para cálculo do DV
+     * @param dezPorX Indica se deve haver substituição de resultado 10 por X
+     * durante o cálculo - padrão usado em alguns lugares
+     * @param quantidadeDigitos Quantidade de dígitos a serem retornados
+     * @return DV gerado.
+     */
+    public static String getDVBase10(String number, boolean dezPorX, int quantidadeDigitos) {
+        char subUm = '0';
+        if (dezPorX) {
+            subUm = 'X';
+        }
+        return getDVBaseParametrizada(number, 10, '0', subUm, quantidadeDigitos);
+    }
+
+    /**
+     * Calcular um dígito verificador a partir de uma sequência de números
+     * enviada. O maior peso usado atinge a base, retorna a 2
+     *
+     * @param number - Sequência de números para cálculo do DV
+     * @param base Valor da base que se deseja usar para o cálculo do DV
+     * @param subZero Caracter que deve substituir o resultado quando o resto
+     * for 0
+     * @param subUm Caracter que deve substituir o resultado quando o resto for
+     * 1
+     * @return DV gerado.
+     */
+    public static String getDVBaseParametrizada(String number, int base, char subZero, char subUm) {
+
+        if (!org.apache.commons.lang.StringUtils.isNumeric(number)) {
+            throw new IllegalArgumentException("Value is not number");
+        }
+
+        int peso = 2;
+        int dv = 0;
+        for (int i = number.length() - 1; i >= 0; i--) {
+            dv += Integer.parseInt(number.substring(i, i + 1)) * peso;
+            if (peso == base - 1) {
+                peso = 2;
+            } else {
+                peso++;
+            }
+        }
+        dv = dv % 11;
+        if (dv > 1) {
+            return String.valueOf(11 - dv);
+        } else if (dv == 1) {
+            return String.valueOf(subUm);
+        }
+        return String.valueOf(subZero);
+
+    }
+
+    /**
+     * Calcular um dígito verificador usando o módulo 11, base 10, com a
+     * quantidade de casas indicadas a partir de uma sequência de números
+     * enviada.
+     *
+     * @param number - Sequência de números para cálculo do DV
+     * @param base Valor da base que se deseja usar para o cálculo do DV
+     * @param subZero Caracter que deve substituir o resultado quando o resto
+     * for 0
+     * @param subUm Caracter que deve substituir o resultado quando o resto for
+     * 1
+     * @param quantidadeDigitos Quantidade de dígitos a serem retornados
+     * @return DV gerado.
+     */
+    public static String getDVBaseParametrizada(String fonte, int base,
+            char subZero, char subUm, int quantidadeDigitos) {
+        if (quantidadeDigitos > 1) {
+            String parcial = getDVBaseParametrizada(fonte, base, subZero, subUm);
+            return parcial + getDVBaseParametrizada(fonte + parcial, base, subZero, subUm, --quantidadeDigitos);
+        } else {
+            return getDVBaseParametrizada(fonte, base, subZero, subUm);
+        }
+    }
+
+    /**
+     * Calcular um dígito verificador a partir de uma sequência de números
+     * enviada e uma constante a ser acrescida ao somatório. O maior peso usado
+     * atinge a base, retorna a 2
+     *
+     * @param number - Sequência de números para cálculo do DV
+     * @param base Valor da base que se deseja usar para o cálculo do DV
+     * @param subZero Caracter que deve substituir o resultado quando o resto
+     * for 0
+     * @param subUm Caracter que deve substituir o resultado quando o resto for
+     * 1
+     * @param constante Valor que deve ser acrescido ao somatório durante o
+     * cálculo
+     * @return DV gerado.
+     */
+    public static String getDVBaseParametrizadaComConstante(String number, int base, char subZero, char subUm, int constante) {
+        if (!org.apache.commons.lang.StringUtils.isNumeric(number)) {
+            throw new IllegalArgumentException("Value is not number");
+        }
+        int peso = 2;
+        int dv = constante;
+        for (int i = number.length() - 1; i >= 0; i--) {
+            dv += Integer.parseInt(number.substring(i, i + 1)) * peso;
+            if (peso == base - 1) {
+                peso = 2;
+            } else {
+                peso++;
+            }
+        }
+        dv = dv % 11;
+        if (dv > 1) {
+            return String.valueOf(11 - dv);
+        } else if (dv == 1) {
+            return String.valueOf(subUm);
+        }
+        return String.valueOf(subZero);
+    }
+
+    public static String getMod11(String string) {
         int d1, d2;
         int digito1, digito2, resto;
         int digitoVerificador;
@@ -60,4 +255,8 @@ public class Mod11 {
         return nDigResult;
     }
 
+    public static void main(String[] args) {
+        System.out.println(getDV("005610443", 2));
+        System.out.println("fim");
+    }
 }
