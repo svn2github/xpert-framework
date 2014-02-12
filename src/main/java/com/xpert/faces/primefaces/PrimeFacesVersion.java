1ffc2d3d105eee9f39160f4466942d24f1c29ff9
ayslanms@gmail.com <ayslanms@gmail.com@e489019e-bc00-2c72-aadb-f79410f744ba>
2014-02-12 17:22:45 +0000

    git-svn-id: http://xpert-framework.googlecode.com/svn/xpert-framework/trunk@257 e489019e-bc00-2c72-aadb-f79410f744ba

--- src/main/java/com/xpert/faces/primefaces/PrimeFacesVersion.java
+++ src/main/java/com/xpert/faces/primefaces/PrimeFacesVersion.java
@@ -13,8 +13,8 @@ public enum PrimeFacesVersion {
     
     VERSION_3("Version 3", "primefaces3"), VERSION_4("Version 4", "primefaces4");
     
-    private String description;
-    private String packageName;
+    private final String description;
+    private final String packageName;
 
     private PrimeFacesVersion(String description, String packageName) {
         this.description = description;
