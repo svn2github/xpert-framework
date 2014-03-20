8b4dcf587f9dd73eaa51f9feffef59c13266871b
arnaldofox@gmail.com <arnaldofox@gmail.com@e489019e-bc00-2c72-aadb-f79410f744ba>
2014-03-20 02:08:40 +0000

    git-svn-id: http://xpert-framework.googlecode.com/svn/xpert-framework/trunk@292 e489019e-bc00-2c72-aadb-f79410f744ba

--- src/main/java/com/xpert/faces/utils/FacesJasper.java
+++ src/main/java/com/xpert/faces/utils/FacesJasper.java
@@ -1,6 +1,7 @@
 package com.xpert.faces.utils;
 
 import com.xpert.jasper.JRBeanCollectionDataSource;
+import java.io.ByteArrayOutputStream;
 import java.util.List;
 import java.util.Map;
 import java.util.logging.Level;
@@ -9,9 +10,11 @@ import javax.faces.context.FacesContext;
 import javax.persistence.EntityManager;
 import net.sf.jasperreports.engine.JREmptyDataSource;
 import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JRExporterParameter;
 import net.sf.jasperreports.engine.JasperExportManager;
 import net.sf.jasperreports.engine.JasperFillManager;
 import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.export.ooxml.JRXlsxExporter;
 
 /**
  *
@@ -38,6 +41,33 @@ public class FacesJasper {
                 jasperPrint = JasperFillManager.fillReport(layout, parameters, jRBeanCollectionDataSource);
             }
             FacesUtils.download(JasperExportManager.exportReportToPdf(jasperPrint), "application/pdf", fileName.endsWith(".pdf") ? fileName : fileName + ".pdf");
+        } catch (JRException ex) {
+            logger.log(Level.SEVERE, null, ex);
+            throw new RuntimeException(ex);
+        }
+    }
+
+    public static void createJasperExcel(List dataSource, Map parameters, String path, String fileName, EntityManager entityManager) {
+
+        try {
+            String layout = FacesContext.getCurrentInstance().getExternalContext().getRealPath(path);
+            JasperPrint jasperPrint;
+            JRBeanCollectionDataSource jRBeanCollectionDataSource = new JRBeanCollectionDataSource(dataSource, entityManager);
+            if (jRBeanCollectionDataSource.getData() == null || jRBeanCollectionDataSource.getData().isEmpty()) {
+                JREmptyDataSource jREmptyDataSource = new JREmptyDataSource();
+                jasperPrint = JasperFillManager.fillReport(layout, parameters, jREmptyDataSource);
+            } else {
+                jasperPrint = JasperFillManager.fillReport(layout, parameters, jRBeanCollectionDataSource);
+            }
+
+            ByteArrayOutputStream xlsReport = new ByteArrayOutputStream();
+            JRXlsxExporter Xlsxexporter = new JRXlsxExporter();
+            Xlsxexporter.setParameter(JRExporterParameter.JASPER_PRINT, jasperPrint);
+            Xlsxexporter.setParameter(JRExporterParameter.OUTPUT_STREAM, xlsReport);
+            Xlsxexporter.setParameter(JRExporterParameter.IGNORE_PAGE_MARGINS, true);
+            Xlsxexporter.exportReport();
+
+            FacesUtils.download(xlsReport.toByteArray(), "application/vnd.ms-excel", fileName.endsWith(".xls") ? fileName : fileName + ".xls");
         } catch (JRException ex) {
             logger.log(Level.SEVERE, null, ex);
             throw new RuntimeException(ex);
