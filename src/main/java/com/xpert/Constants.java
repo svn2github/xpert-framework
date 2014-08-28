ae9088b354e1b5fa6a07009970fb471b3b052a71
ayslanms@gmail.com <ayslanms@gmail.com@e489019e-bc00-2c72-aadb-f79410f744ba>
2014-08-28 13:45:54 +0000

    git-svn-id: http://xpert-framework.googlecode.com/svn/xpert-framework/trunk@391 e489019e-bc00-2c72-aadb-f79410f744ba

--- src/main/java/com/xpert/Constants.java
+++ src/main/java/com/xpert/Constants.java
@@ -6,7 +6,7 @@ package com.xpert;
  */
 public class Constants {
 
-    public static final String VERSION = "1.7.1-SNAPSHOT";
+    public static final String VERSION = "1.7.1";
     public static final String USER_ROLES = "userRoles";
     public static final String USER_ROLES_KEY_MAP = "userRolesKeyMap";
     public static final String USER_ROLES_URL_MAP = "userRolesUrlMap";
