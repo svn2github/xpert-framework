501446d04d6eadedadb942999f2394cc2ce4941a
ayslanms@gmail.com <ayslanms@gmail.com@e489019e-bc00-2c72-aadb-f79410f744ba>
2014-11-29 16:12:24 +0000

    git-svn-id: http://xpert-framework.googlecode.com/svn/xpert-framework/trunk@521 e489019e-bc00-2c72-aadb-f79410f744ba

---
+++ src/main/java/com/xpert/utils/DateUtils.java
@@ -0,0 +1,52 @@
+package com.xpert.utils;
+
+import java.util.Calendar;
+import java.util.Date;
+
+/**
+ * Ulity class to use with java.util.Date and java.util.Calendar
+ *
+ * @author Ayslan
+ */
+public class DateUtils {
+
+    /**
+     * Return the year of a date.
+     * Example: '2014-01-02' (yyyy-MM-dd) returns '2014'
+     * 
+     * @param date
+     * @return 
+     */
+    public static int getYear(Date date) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        return calendar.get(Calendar.YEAR);
+    }
+    
+    /**
+     * Return the month of a date.
+     * Example: '2014-01-02' (yyyy-MM-dd) returns '01'
+     * 
+     * @param date
+     * @return 
+     */
+    public static int getMonth(Date date) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        return calendar.get(Calendar.MONTH);
+    }
+    
+    /**
+     * Return the day of a date.
+     * Example: '2014-01-02' (yyyy-MM-dd) returns '02'
+     * 
+     * @param date
+     * @return 
+     */
+    public static int getDay(Date date) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        return calendar.get(Calendar.DATE);
+    }
+
+}
