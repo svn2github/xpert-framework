90c0fcc367a5ebda90c47e837a914a769348cc1b
ayslanms@gmail.com <ayslanms@gmail.com@e489019e-bc00-2c72-aadb-f79410f744ba>
2015-01-05 15:15:37 +0000

    git-svn-id: http://xpert-framework.googlecode.com/svn/xpert-framework/trunk@558 e489019e-bc00-2c72-aadb-f79410f744ba

--- src/main/java/com/xpert/faces/bean/Xpert.java
+++ src/main/java/com/xpert/faces/bean/Xpert.java
@@ -3,13 +3,14 @@ package com.xpert.faces.bean;
 import com.xpert.faces.primefaces.PrimeFacesUtils;
 
 /**
+ * Generic managed bean to acess utils methods and funtions from xpert-framework
  *
  * @author Ayslan
  */
 public class Xpert {
- 
-    public String normalizePrimeFacesWidget(String widgetVar){
+
+    public String normalizePrimeFacesWidget(String widgetVar) {
         return PrimeFacesUtils.normalizeWidgetVar(widgetVar);
     }
-    
+
 }
